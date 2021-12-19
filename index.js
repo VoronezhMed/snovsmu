187d1df0ac47c539224f45b2c40eb7e82e0ddd13
VoronezhMed <89032414+VoronezhMed@users.noreply.github.com>
2021-12-19 18:35:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,3 +28,9 @@ async function start() {
 }
 
 start()
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
